d833c1211370ef9096ff155edf9ca737fa872415
Donthireddysivakumari <167413914+Donthireddysivakumari@users.noreply.github.com>
2025-05-28 09:39:29 +0530

    Add files via upload

---
+++ Dob.pl
@@ -0,0 +1,7 @@
+% person(Name, DateOfBirth).
+
+person('Alice', date(1998, 5, 21)).
+person('Bob', date(2001, 12, 3)).
+person('Charlie', date(1995, 7, 14)).
+person('Diana', date(2000, 1, 30)).
+person('Eve', date(1999, 11, 9)).
